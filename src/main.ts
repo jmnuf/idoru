2d005ceb2d509921d84ff85230a38b4a911e9489
Jose Marco Nufio Garrido <jose.m.nufio.0@gmail.com>
2023-08-16 20:40:04 +0200

    Hacky Fix: Add delay before calling UI.create since model has yet to be set within norishre. Wait till this get patched in lib

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
 import { missNorishre } from "@jmnuf/norishre";
 import { UI } from "@peasy-lib/peasy-ui";
 import create_navbar from "./components/navbar";
-import { PageModel as IndexModel } from "./pages/index";
 import { invoke } from "@tauri-apps/api/tauri";
 
 const norishre = missNorishre({
 	index: {
 		path: "/",
-		model: IndexModel(),
+		model: async () => {
+			const { PageModel } = await import("./pages/index");
+			const model = PageModel();
+			return model;
+		},
 	},
 	peekFolder: {
 		path: "/peeker",
@@ -15,9 +18,14 @@ const norishre = missNorishre({
 			const { PageModel } = await import("./pages/peek-folder");
 			const model = PageModel();
 			try {
-				const path = await invoke<string>("relative_to_full_path", { relativePath: "./" });
+				let path = await invoke<string>("relative_to_full_path", { relativePath: "./" });
 				console.log("Transforming", "'./'", "to", path);
 				if (typeof path == "string") {
+					// Hit this in windows no idea what it is but it's ugly
+					if (path.startsWith("\\\\?\\")) {
+						path = path.replace("\\\\?\\", "");
+					}
+					path = path.replace(/\\/g, "/");
 					model.base_directory = path;
 					model.directory = path;
 				}
@@ -27,15 +35,35 @@ const norishre = missNorishre({
 			model.do_search(false);
 			return model;
 		}
+	},
+	testPage: {
+		path: "/testing",
+		async model() {
+			const { PageModel } = await import("./pages/test");
+			const model = PageModel();
+			return model;
+		},
 	}
 });
 const navbar = create_navbar(norishre);
 navbar.add_page("index", "Home");
 navbar.add_page("peekFolder", "Peeker");
+navbar.add_page("testPage", "debug");
 UI.create(document.body, navbar, navbar.template);
 
 const [active_id, params] = norishre.find_arrow_id_by_url();
 
+console.log("Found arrow data:", { active_id, params });
+
 norishre.pull_from_quiver(active_id as any, params).then(() => {
-	UI.create(document.body, norishre, norishre.template);
+	if (!norishre.pulled_arrow) {
+		setTimeout(load_page, 500);
+	} else {
+		load_page();
+	}
 });
+
+async function load_page() {
+	UI.create(document.body, norishre, norishre.template);
+	console.log("Loaded model for arrow with id", active_id);
+}
